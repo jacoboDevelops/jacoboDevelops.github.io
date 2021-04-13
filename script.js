fae7a8b3248e4a908f0dc6fcc09ebfce110143ac
jacoboDevelops <61711832+jacoboDevelops@users.noreply.github.com>
2021-04-13 17:40:27 +0200

    First step. Basic css basic html

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+
+function omnie() {
+    document.getElementById("omniecont").style.display= "block";
+    document.getElementById("projekt").style.display= "none";
+    document.getElementById("kontakt").style.display= "none";
+
+
+}
+
+function projekty() {
+    
+    document.getElementById("omniecont").style.display= "none";
+    document.getElementById("projekt").style.display= "block";
+    document.getElementById("kontakt").style.display= "none";
+
+}
+
+function kontakt() {
+    document.getElementById("omniecont").style.display= "none";
+    document.getElementById("projekt").style.display= "none";
+    document.getElementById("kontakt").style.display= "block";
+}
+
